f06a131bd60b45eb13acaece06ab358ff6bb4c51
Adam Stokes <adam.stokes@ubuntu.com>
2014-10-30 22:05:46 -0400

    fix conditional
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- lib/Pika/Plugin/Launchpad.pm
+++ lib/Pika/Plugin/Launchpad.pm
@@ -53,29 +53,31 @@ method irc_privmsg ($msg) {
     {
         $bug = $self->_get_bug($bug_id[1]);
     }
-    if ((@bug_id) = $msg->message =~ m/(bug|#|LP|lp)\s*#?(\d{6,})/i) {
+    elsif ((@bug_id) = $msg->message =~ m/(bug|#|LP|lp)\s*#?(\d{6,})/i) {
         $bug = $self->_get_bug($bug_id[1]);
     }
-    $self->do_notice(
-        {   channel => $msg->channel,
-            message => join(
-                " ",
-                '('
-                  . UNDERLINE
-                  . BOLD
-                  . $bug->{result}->{information_type}
-                  . NORMAL . ')',
-                $bug->{result}->{title},
-                '['
-                  . BOLD
-                  . $bug->{task}->{importance} . ','
-                  . $bug->{task}->{status}
-                  . BOLD . ']',
+    if ($bug) {
+        $self->do_notice(
+            {   channel => $msg->channel,
+                message => join(
+                    " ",
+                    '('
+                      . UNDERLINE
+                      . BOLD
+                      . $bug->{result}->{information_type}
+                      . NORMAL . ')',
+                    $bug->{result}->{title},
+                    '['
+                      . BOLD
+                      . $bug->{task}->{importance} . ','
+                      . $bug->{task}->{status}
+                      . BOLD . ']',
 
-                $bug->{result}->{web_link}
-            )
-        }
-    );
+                    $bug->{result}->{web_link}
+                )
+            }
+        );
+    }
     return $self->pass;
 }
 
